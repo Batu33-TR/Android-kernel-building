f4d19896afba07e4c19b737a45fa6d0146455e43
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-06-03 17:10:23 +0300

    Disable Auto anykernel3 compression

--- kernel_build.sh
+++ kernel_build.sh
@@ -5,8 +5,8 @@
 # Set defaults directory's
 ROOT_DIR=$(pwd)
 OUT_DIR=$ROOT_DIR/out
-ANYKERNEL_DIR=$ROOT_DIR/anykernel3
-KERNEL_DIR=$ROOT_DIR/kernel
+# ANYKERNEL_DIR=$ROOT_DIR/anykernel3
+KERNEL_DIR=$ROOT_DIR
 DATE=$(date +"%m-%d-%y")
 BUILD_START=$(date +"%s")
 
@@ -15,15 +15,15 @@ export ARCH=arm64
 export SUBARCH=arm64
 
 # Set kernel name and defconfig
-export VERSION=SebaUbuntu-KernelName-v1
-export DEFCONFIG=whyred_defconfig
+export VERSION=MedusaKernel-VersionNumber-VersionName
+export DEFCONFIG=j4plus_kali_defconfig
 
 # Keep it as is
 export LOCALVERSION=-$VERSION
 
 # Export Username and machine name
-export KBUILD_BUILD_USER=SebaUbuntu
-export KBUILD_BUILD_HOST=Seba-PC
+export KBUILD_BUILD_USER=Batuu
+export KBUILD_BUILD_HOST=Manjaro
 
 # Color definition
 red=`tput setaf 1`
@@ -115,67 +115,71 @@ if [ $BUILD_SUCCESS != 0 ]
 		exit
 fi
 
-# Possible kernel filenames
-APPENDED_DTB_KERNEL="zImage-dtb Image.gz-dtb Image.bz2-dtb Image.lzo-dtb Image.lzma-dtb Image.xz-dtb Image.lz4-dtb Image-dtb"
-NOT_APPENDED_DTB_KERNEL="zImage Image.gz Image.bz2 Image.lzo Image.lzma Image.xz Image.lz4 Image.fit Image"
-KERNEL_DTB="dt dt.img dtb dtb.img"
+# # Possible kernel filenames
+# APPENDED_DTB_KERNEL="zImage-dtb Image.gz-dtb Image.bz2-dtb Image.lzo-dtb Image.lzma-dtb Image.xz-dtb Image.lz4-dtb Image-dtb"
+# NOT_APPENDED_DTB_KERNEL="zImage Image.gz Image.bz2 Image.lzo Image.lzma Image.xz Image.lz4 Image.fit Image"
+# KERNEL_DTB="dt dt.img dtb dtb.img"
+# 
+# cd "$ROOT_DIR"
+# 
+# 
 
-cd "$ROOT_DIR"
-
-clear
 echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds $reset"
-echo -e "Making flashable zip using anykernel3"
+exit
 
-# remove all zips and old kernels
-rm "$ANYKERNEL_DIR/*.zip"
-for i in "$APPENDED_DTB_KERNEL $NOT_APPENDED_DTB_KERNEL $KERNEL_DTB"
-	do
-		if [ -f "$ANYKERNEL_DIR/$i" ]
-			then
-				rm -f "$ANYKERNEL_DIR/$i"
-		fi
-done
-
-# First of all see if kernel with appended dtb exists and copy it
-for i in "$APPENDED_DTB_KERNEL"
-	do
-		if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
-			then
-				cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
-				KERNEL_HAVE_APPENDED_DTB=true
-		fi
-		[ $KERNEL_HAVE_APPENDED_DTB ] && break
-done
-
-# If kernel with appended dtbs doesn't exists, copy kernel w/o appended dtb and dtb image
-if [ "$KERNEL_HAVE_APPENDED_DTB" != true ]
-	then
-		for i in $NOT_APPENDED_DTB_KERNEL
-			do
-				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
-					then
-						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
-						KERNEL_HAVE_APPENDED_DTB=false
-				fi
-				[ $KERNEL_HAVE_APPENDED_DTB = false ] && break
-		done
-		for i in $KERNEL_DTB
-			do
-				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
-					then
-						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
-				fi
-		done
-fi
-
-# Change the directory to anykernel3 directory
-cd $ANYKERNEL_DIR
-	
-# Build a flashable zip Device using anykernel2
-zip -r9 $VERSION-$DATE.zip $ANYKERNEL_DIR/* -x $ANYKERNEL_DIR/README $ANYKERNEL_DIR/$VERSION-$DATE.zip
-
-# Exported variables cleanup
-for i in ARCH SUBARCH LOCALVERSION KBUILD_BUILD_USER KBUILD_BUILD_HOST DEFCONFIG CROSS_COMPILE CROSS_COMPILE_ARM32 CROSS_COMPILE_X86
-	do
-		unset $i
-done
+# echo -e "Making flashable zip using anykernel3"
+# 
+# # remove all zips and old kernels
+# rm "$ANYKERNEL_DIR/*.zip"
+# for i in "$APPENDED_DTB_KERNEL $NOT_APPENDED_DTB_KERNEL $KERNEL_DTB"
+# 	do
+# 		if [ -f "$ANYKERNEL_DIR/$i" ]
+# 			then
+# 				rm -f "$ANYKERNEL_DIR/$i"
+# 		fi
+# done
+# 
+# # First of all see if kernel with appended dtb exists and copy it
+# for i in "$APPENDED_DTB_KERNEL"
+# 	do
+# 		if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+# 			then
+# 				cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+# 				KERNEL_HAVE_APPENDED_DTB=true
+# 		fi
+# 		[ $KERNEL_HAVE_APPENDED_DTB ] && break
+# done
+# 
+# # If kernel with appended dtbs doesn't exists, copy kernel w/o appended dtb and dtb image
+# if [ "$KERNEL_HAVE_APPENDED_DTB" != true ]
+# 	then
+# 		for i in $NOT_APPENDED_DTB_KERNEL
+# 			do
+# 				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+# 					then
+# 						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+# 						KERNEL_HAVE_APPENDED_DTB=false
+# 				fi
+# 				[ $KERNEL_HAVE_APPENDED_DTB = false ] && break
+# 		done
+# 		for i in $KERNEL_DTB
+# 			do
+# 				if [ -f "$OUT_DIR/arch/$ARCH/boot/$i" ]
+# 					then
+# 						cp "$OUT_DIR/arch/$ARCH/boot/$i" "$ANYKERNEL_DIR/$i"
+# 				fi
+# 		done
+# fi
+# 
+# # Change the directory to anykernel3 directory
+# cd $ANYKERNEL_DIR
+# 	
+# # Build a flashable zip Device using anykernel2
+# zip -r9 $VERSION-$DATE.zip $ANYKERNEL_DIR/* -x $ANYKERNEL_DIR/README $ANYKERNEL_DIR/$VERSION-$DATE.zip
+# 
+# # Exported variables cleanup
+# for i in ARCH SUBARCH LOCALVERSION KBUILD_BUILD_USER KBUILD_BUILD_HOST DEFCONFIG CROSS_COMPILE CROSS_COMPILE_ARM32 CROSS_COMPILE_X86
+# 	do
+# 		unset $i
+# done
+# 
